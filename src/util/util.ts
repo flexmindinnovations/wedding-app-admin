437fe09014b43c2afac4cd6681b64cdbb42c2473
Imran <flexmindinnovations@outlook.com>
2024-02-26 14:45:51 +0530

    Branch service integrated

--- src/util/util.ts
+++ src/util/util.ts
@@ -1,10 +1,10 @@
 export const COLOR_SCHEME = 'br';
 
-const redButton = `flex item-center justify-start disabled:bg-gray-200 disabled:cursor-not-allowed disabled:border-gray-300 text-white bg-red-500 hover:bg-red-600 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm text-center items-center justify-start gap-2 me-2 dark:bg-gray-600 dark:hover:bg-gray-500 dark:focus:ring-gray-400`;
+const redButton = `flex item-center justify-start disabled:bg-gray-200 disabled:cursor-not-allowed disabled:border-gray-300 text-white bg-red-500 hover:bg-red-600 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm text-center items-center justify-start gap-2 me-2 dark:bg-gray-600 dark:hover:bg-gray-500 disabled:dark:bg-gray-800 disabled:dark:opacity-70 disabled:dark:shadow-none disabled:bg-gray-300 disabled:opacity-70 disabled:shadow-none disabled:hover:bg-gray-300 dark:focus:ring-gray-400`;
 
-const boButton = `flex item-center justify-start disabled:bg-gray-200 disabled:cursor-not-allowed disabled:border-gray-300 text-white bg-bo-500 hover:bg-bo-600 focus:ring-4 focus:outline-none focus:ring-bo-300 font-medium rounded-lg text-sm text-center items-center justify-start gap-2 me-2 dark:bg-gray-600 dark:hover:bg-gray-500 dark:focus:ring-gray-400`;
+const boButton = `flex item-center justify-start disabled:bg-gray-200 disabled:cursor-not-allowed disabled:border-gray-300 text-white bg-bo-500 hover:bg-bo-600 focus:ring-4 focus:outline-none focus:ring-bo-300 font-medium rounded-lg text-sm text-center items-center justify-start gap-2 me-2 dark:bg-gray-600 dark:hover:bg-gray-500 disabled:dark:bg-gray-800 disabled:dark:opacity-70 disabled:dark:shadow-none disabled:bg-gray-300 disabled:opacity-70 disabled:shadow-none disabled:hover:bg-gray-300 dark:focus:ring-gray-400`;
 
-const brButton = `flex item-center justify-start disabled:bg-gray-200 disabled:cursor-not-allowed disabled:border-gray-300 text-white bg-br-500 hover:bg-br-600 focus:ring-4 focus:outline-none focus:ring-br-300 font-medium rounded-lg text-sm text-center items-center justify-start gap-2 me-2 dark:bg-gray-600 dark:hover:bg-gray-500 dark:focus:ring-gray-400`;
+const brButton = `flex item-center justify-start disabled:bg-gray-200 disabled:cursor-not-allowed disabled:border-gray-300 text-white bg-br-500 hover:bg-br-600 focus:ring-4 focus:outline-none focus:ring-br-300 font-medium rounded-lg text-sm text-center items-center justify-start gap-2 me-2 dark:bg-gray-600 dark:hover:bg-gray-500 disabled:dark:bg-gray-800 disabled:dark:opacity-70 disabled:dark:shadow-none disabled:bg-gray-300 disabled:opacity-70 disabled:shadow-none disabled:hover:bg-gray-300 dark:focus:ring-gray-400`;
 
 
 export const themeVariables: any = {
@@ -64,43 +64,43 @@ export const stepperThemeVariables: any = {
         },
         bg: {
             active: `flex items-center justify-center w-10 h-10 bg-${COLOR_SCHEME}-100 rounded-full lg:h-12 lg:w-12 dark:bg-${COLOR_SCHEME}-800 shrink-0`,
-            inactive: `flex items-center justify-center w-10 h-10 bg-gray-100 rounded-full lg:h-12 lg:w-12 dark:bg-gray-700 shrink-0`,
+            inactive: `flex items-center justify-center w-10 h-10 bg-gray-100 rounded-full lg:h-12 lg:w-12 dark:bg-gray-800 shrink-0`,
             complete: `bg-green-200`,
         }
     },
     red: {
         line: {
             active: `flex w-full items-center text-red-600 dark:text-red-500 after:content-[''] after:w-full after:h-1 after:border-b after:border-red-100 after:border-4 after:inline-block dark:after:border-red-800`,
-            inactive: `flex w-full items-center after:content-[''] after:w-full after:h-1 after:border-b after:border-gray-100 after:border-4 after:inline-block dark:after:border-gray-700`,
+            inactive: `flex w-full items-center after:content-[''] after:w-full after:h-1 after:border-b after:border-gray-100 after:border-4 after:inline-block dark:after:border-gray-800`,
             complete: `flex w-full items-center text-green-600 dark:text-green-500 after:content-[''] after:w-full after:h-1 after:border-b after:border-green-100 after:border-4 after:inline-block dark:after:border-green-800`
         },
         bg: {
             active: `flex items-center justify-center w-10 h-10 bg-red-100 rounded-full lg:h-12 lg:w-12 dark:bg-red-800 shrink-0`,
-            inactive: `flex items-center justify-center w-10 h-10 bg-gray-100 rounded-full lg:h-12 lg:w-12 dark:bg-gray-700 shrink-0`,
+            inactive: `flex items-center justify-center w-10 h-10 bg-gray-100 rounded-full lg:h-12 lg:w-12 dark:bg-gray-800 shrink-0`,
             complete: `flex items-center justify-center w-10 h-10 bg-green-100 rounded-full lg:h-12 lg:w-12 dark:bg-green-800 shrink-0`
         }
     },
     bo: {
         line: {
             active: `flex w-full items-center text-bo-600 dark:text-bo-500 after:content-[''] after:w-full after:h-1 after:border-b after:border-bo-100 after:border-4 after:inline-block dark:after:border-bo-800`,
-            inactive: `flex w-full items-center after:content-[''] after:w-full after:h-1 after:border-b after:border-gray-100 after:border-4 after:inline-block dark:after:border-gray-700`,
+            inactive: `flex w-full items-center after:content-[''] after:w-full after:h-1 after:border-b after:border-gray-100 after:border-4 after:inline-block dark:after:border-gray-800`,
             complete: `flex w-full items-center text-green-600 dark:text-green-500 after:content-[''] after:w-full after:h-1 after:border-b after:border-green-100 after:border-4 after:inline-block dark:after:border-green-800`
         },
         bg: {
             active: `flex items-center justify-center w-10 h-10 bg-bo-100 rounded-full lg:h-12 lg:w-12 dark:bg-bo-800 shrink-0`,
-            inactive: `flex items-center justify-center w-10 h-10 bg-gray-100 rounded-full lg:h-12 lg:w-12 dark:bg-gray-700 shrink-0`,
+            inactive: `flex items-center justify-center w-10 h-10 bg-gray-100 rounded-full lg:h-12 lg:w-12 dark:bg-gray-800 shrink-0`,
             complete: `flex items-center justify-center w-10 h-10 bg-green-100 rounded-full lg:h-12 lg:w-12 dark:bg-green-800 shrink-0`
         }
     },
     br: {
         line: {
             active: `flex w-full items-center text-br-600 dark:text-br-500 after:content-[''] after:w-full after:h-1 after:border-b after:border-br-100 after:border-4 after:inline-block dark:after:border-br-800`,
-            inactive: `flex w-full items-center after:content-[''] after:w-full after:h-1 after:border-b after:border-gray-100 after:border-4 after:inline-block dark:after:border-gray-700`,
+            inactive: `flex w-full items-center after:content-[''] after:w-full after:h-1 after:border-b after:border-gray-100 after:border-4 after:inline-block dark:after:border-gray-800`,
             complete: `flex w-full items-center text-green-600 dark:text-green-500 after:content-[''] after:w-full after:h-1 after:border-b after:border-green-100 after:border-4 after:inline-block dark:after:border-green-800`
         },
         bg: {
             active: `flex items-center justify-center w-10 h-10 bg-br-100 rounded-full lg:h-12 lg:w-12 dark:bg-br-800 shrink-0`,
-            inactive: `flex items-center justify-center w-10 h-10 bg-gray-100 rounded-full lg:h-12 lg:w-12 dark:bg-gray-700 shrink-0`,
+            inactive: `flex items-center justify-center w-10 h-10 bg-gray-100 rounded-full lg:h-12 lg:w-12 dark:bg-gray-800 shrink-0`,
             complete: `flex items-center justify-center w-10 h-10 bg-green-100 rounded-full lg:h-12 lg:w-12 dark:bg-green-800 shrink-0`
         }
     }
