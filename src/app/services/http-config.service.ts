d30abbb190a9935674abb7898c116c01b484509d
Imran <flexmindinnovations@outlook.com>
2024-02-26 15:35:49 +0530

    minor changes

--- src/app/services/http-config.service.ts
+++ src/app/services/http-config.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable, inject } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, catchError, throwError } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -13,22 +13,47 @@ export class HttpConfigService {
 
   get(url: string): Observable<any> {
     return this.http.get(url, { headers: this.getHeaders({ isImage: false }) })
+    .pipe(
+      catchError((error) => {
+        return throwError(() => error)
+      })
+    );
   }
 
   post(url: string, payload: any): Observable<any> {
     return this.http.post(url, payload, { headers: this.getHeaders({ isImage: false }) })
+    .pipe(
+      catchError((error) => {
+        return throwError(() => error)
+      })
+    );
   }
 
   put(url: string, payload: any): Observable<any> {
     return this.http.put(url, payload, { headers: this.getHeaders({ isImage: false }) })
+    .pipe(
+      catchError((error) => {
+        return throwError(() => error)
+      })
+    );
   }
 
   delete(url: string): Observable<any> {
     return this.http.delete(url, { headers: this.getHeaders({ isImage: false }) })
+    .pipe(
+      catchError((error) => {
+        return throwError(() => error)
+      })
+    );
   }
 
-  postImage(url: string): Observable<any> {
-    return this.http.get(url, { headers: this.getHeaders({ isImage: true }) })
+  postImage(url: string, payload: any): Observable<any> {
+    return this.http.post(url, payload, { headers: this.getHeaders({ isImage: true }) })
+    .pipe(
+      catchError((error) => {
+        return throwError(() => error)
+      })
+    );
   }
 
   getHeaders({ isImage }: { isImage: boolean }): HttpHeaders {
