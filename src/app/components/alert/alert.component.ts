887075280fb7f1cee22f3c76c533a3a1a1714926
Imran <flexmindinnovations@outlook.com>
2024-02-29 15:38:59 +0530

    Alert autoHide

--- src/app/components/alert/alert.component.ts
+++ src/app/components/alert/alert.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit, inject } from '@angular/core';
 import { AlertType } from 'src/app/enums/alert-types';
 import { AlertService } from 'src/app/services/alert/alert.service';
+import { Dismiss } from "flowbite";
+import type { DismissOptions, DismissInterface } from "flowbite";
+import type { InstanceOptions } from 'flowbite';
 
 @Component({
   selector: 'app-alert',
@@ -20,7 +23,37 @@ export class AlertComponent implements OnInit {
       this.alertType = alert?.type;
       this.alertMessage = alert?.message;
       this.alertId = `alert-${this.alertType}`;
+
+      setTimeout(() => {
+        this.autoHideAlert();
+      }, 3500);
     })
+  }
+
+  autoHideAlert() {
+    const alertEl = document.getElementById('alertContainer');
+    alertEl?.classList.toggle('show');
+  }
+
+  handleHideAlert() {
+    const targetEl: HTMLElement | any = document.getElementById(this.alertId);
+    const triggerEl: HTMLElement | any = document.getElementById(`hide-${this.alertId}`);
+    const options: DismissOptions = {
+      transition: 'transition-opacity',
+      duration: 1000,
+      timing: 'ease-out',
+
+      onHide: (context, targetEl) => {
+        console.log('element has been dismissed')
+        console.log(targetEl)
+      }
+    };
+    const instanceOptions: InstanceOptions = {
+      id: 'targetElement',
+      override: true
+    };
+    const dismiss: DismissInterface = new Dismiss(targetEl, triggerEl, options, instanceOptions);
+    dismiss.hide();
   }
 
 }
